9452eefaa8f0be1c87f8fdab160ce5f62c13ec0c
Christian Echegaray <83435740+clennone@users.noreply.github.com>
2021-08-19 22:25:16 -0500

    1.0.0

---
+++ src/server/index.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const PORT = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+app.use(express.static('dist'));
+
+app.listen(PORT, () => console.log(`APP Travel on server ${PORT}`));
+
+
+
+
